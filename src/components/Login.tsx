9c4289900f29e9d829020fd963d01730feed8e43
Joshua Brown <joshua.rd.brown@gmail.com>
2020-05-16 19:20:35 +0100

    fix indentation

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Login: React.FC = () => {
   return(
-  	<div className='login-container'>
-			<div className='login-container_header'>
-				<span>BlogIT</span>
-				<input 
-					placeholder='FAQs...'
-				/>
-				<div className='login-container_header-buttons-container'>
-					<button>Login</button>
-					<button>Sign up</button>
-				</div>
-			</div>
+    <div className='login-container'>
+      <div className='login-container_header'>
+	<span>BlogIT</span>
+	<input 
+	  placeholder='FAQs...'
+	/>
+	<div className='login-container_header-buttons-container'>
+	  <button>Login</button>
+	  <button>Sign up</button>
+	</div>
+      </div>
     </div>
   )
 
